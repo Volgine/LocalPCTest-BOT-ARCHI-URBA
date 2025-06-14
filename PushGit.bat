9f9608ffc996d1286babe700af0b996c3f6b90dc
unknown <yanis-necer@hotmail.fr>
2025-06-14 08:52:43 +0200

    maj

--- PushGit.bat
+++ PushGit.bat
@@ -1,12 +1,26 @@
 @echo off
-set COMMENT=auto: push depuis script
-set BRANCH=dev
+setlocal ENABLEEXTENSIONS
+
+for /f "tokens=*" %%i in ('git rev-parse --abbrev-ref HEAD') do set BRANCH=%%i
+
+set /p COMMENT=[GIT] Message du commit : 
 
 echo [GIT] Ajout des fichiers...
 git add .
 
-echo [GIT] Commit avec message : %COMMENT%
+:: Vérifie s’il y a quelque chose à commit
+for /f %%C in ('git diff --cached --name-only') do set HASCHANGES=1
+if not defined HASCHANGES (
+    echo [GIT] Rien à commit. Arret.
+    pause
+    exit /b
+)
+
+echo [GIT] Commit sur la branche %BRANCH% avec message : %COMMENT%
 git commit -m "%COMMENT%"
+
+echo [GIT] Pull + rebase avant push...
+git pull origin %BRANCH% --rebase
 
 echo [GIT] Push vers la branche : %BRANCH%
 git push origin %BRANCH%
